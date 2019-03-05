bdfe0cf987892085be4efff393f480461eb7a0c4
Petr <xflajs00@stud.fit.vutbr.cz>
2019-03-05 11:50:42 +0100

    GUI refactoring, add slider for diffraction pattern intesity

--- src/main/java/cz/vutbr/fit/xflajs00/fyo/Controller.kt
+++ src/main/java/cz/vutbr/fit/xflajs00/fyo/Controller.kt
@@ -6,6 +6,7 @@ import javafx.scene.control.Slider
 import javafx.scene.control.Spinner
 import javafx.scene.control.TextField
 import javafx.scene.layout.Pane
+import javafx.util.converter.NumberStringConverter
 import org.jfree.chart.ChartFactory
 import org.jfree.chart.plot.PlotOrientation
 import org.jfree.data.category.DefaultCategoryDataset
@@ -29,7 +30,11 @@ class Controller {
     @FXML
     private var slitDistInput: TextField? = null
     @FXML
+    private var projectDistInput: TextField? = null
+    @FXML
     private var projectDistSlider: Slider? = null
+    @FXML
+    private var intensitySlider: Slider? = null
 
 
     @FXML
@@ -43,6 +48,8 @@ class Controller {
         wavelengthInput?.textProperty()?.bindBidirectional(wavelengthSlider?.valueProperty(), NumberIntStringConverter())
 
         wavelengthSlider?.valueProperty()?.addListener { _ -> test() }
+
+        projectDistInput?.textProperty()?.bindBidirectional(projectDistSlider?.valueProperty(), NumberStringConverter())
         projectDistSlider?.valueProperty()?.addListener { _ -> test() }
 
         intensityCanvas?.widthProperty()?.bind(intensityPane?.widthProperty())
@@ -52,6 +59,26 @@ class Controller {
         intensityCanvas?.heightProperty()?.addListener { _ -> test() }
 
         slitCountInput?.valueProperty()?.addListener { _ -> test() }
+
+        slitWidthInput?.textProperty()?.addListener { _, oldValue, newValue ->
+            if (newValue.toDoubleOrNull() == null) {
+                slitWidthInput?.text = oldValue
+            }
+            if (newValue.toDouble() >= slitDistInput!!.text.toDouble()) {
+                slitWidthInput?.text = oldValue
+            }
+        }
+        slitDistInput?.textProperty()?.addListener { _, oldValue, newValue ->
+            if (newValue.toDoubleOrNull() == null) {
+                slitDistInput?.text = oldValue
+            }
+            if (newValue.toDouble() <= slitWidthInput!!.text.toDouble()) {
+                slitDistInput?.text = oldValue
+            }
+        }
+        slitWidthInput?.textProperty()?.addListener { _ -> test() }
+        slitDistInput?.textProperty()?.addListener { _ -> test() }
+        intensitySlider?.valueProperty()?.addListener { _ -> test() }
     }
 
     fun firstShow() {
@@ -70,7 +97,7 @@ class Controller {
         val step = (second - first) / 10000
         val tmp = t.calcInterval(first, second, step)
 
-        drawIntensity(intensityCanvas!!, tmp, t.λ, 20.0)
+        drawIntensity(intensityCanvas!!, tmp, t.λ, intensitySlider!!.value)
 
         /*val lineChartDataSet = DefaultCategoryDataset()
         var x = first
